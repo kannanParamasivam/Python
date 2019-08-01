1eab0b8aaa62981ff7fd8b9ea34f0842b831ccc3
Kannan Paramasivam <kannan.paramasivam@live.comx>
2019-08-01 07:04:09 -0500

    Adding constructor parameter

--- object_oriented/employee.py
+++ object_oriented/employee.py
@@ -1,10 +1,20 @@
 class Employee:
-    def __init__(self, name, age, phone, city):
-        self.name = name
+    def __init__(self, firstName, lastName, age, phone, city):
+        self.firstName = firstName
+        self.lastName = lastName
         self.age = age
         self.phone = phone
         self.city = city
 
+    def getfullname(self):
+        return "{fn} {ln}".format(fn=self.firstName, ln=self.lastName)
 
-emp1 = Employee('Kannan', 32, '224-280-2046', 'Chicago')
-print('name: {name} \n age: {age}'.format(name=emp1.name, age=emp1.age))
+
+emp1 = Employee('Kannan', 'Paramasivam', 32, '224-280-2046', 'Chicago')
+
+print('First Name: {fn} \nLast Name: {ln}'.format(
+    fn=emp1.firstName, ln=emp1.lastName))
+
+# Two ways of calling instance specific methods
+print('Full Name: {}'.format(emp1.getfullname()))
+print('Full Name: {}'.format(Employee.getfullname(emp1)))
